7c503af86ed2f276e7b466d8457d3fb161b1a5b7
Izet Delibasic <izetdelibasic55@gmail.com>
2024-05-12 20:06:35 +0200

    Adding navigation menu for small screens

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaLocationArrow } from "react-icons/fa";
 import { useDashboardContext } from "../../pages/Dashboard/Dashboard";
 import { Links } from "../../constants/LinksConstant";
 import { useNavigate } from "react-router-dom";
 import { MdLogout } from "react-icons/md";
+import { FiMenu } from "react-icons/fi";
+import { FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
   const { firstName, lastName, email, phoneNumber } = useDashboardContext();
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="bg-blueColor border-b-2 border-black border-opacity-20 font-montserrat">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -20,13 +28,20 @@ const Navbar = () => {
             Jumpin
           </div>
         </a>
-        <div className="hidden w-full md:block md:w-auto">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="text-white">
+            {isOpen ? <FaTimes className="" /> : <FiMenu className="" />}
+          </button>
+        </div>
+        <div
+          className={`w-full md:w-auto md:flex ${isOpen ? "block" : "hidden"}`}
+        >
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             {Links.map((link, index) => (
               <li key={index}>
                 <a
                   href={link.href}
-                  className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-bluePurple md:p-0"
+                  className="block py-2 px-3 text-white rounded hover:bg-gray-100 ease-in-out duration-300 hover:text-blueColor md:hover:bg-transparent md:border-0 md:hover:text-bluePurple md:p-0"
                 >
                   {link.title}
                 </a>
@@ -38,7 +53,7 @@ const Navbar = () => {
                 window.location.href = "/";
               }}
             >
-              <MdLogout className="text-white" />
+              <MdLogout className="text-white mt-6 md:mt-0 mx-auto" />
             </button>
           </ul>
         </div>
