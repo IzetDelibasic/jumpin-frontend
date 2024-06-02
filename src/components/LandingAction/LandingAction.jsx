23fd205b8221b6c4a8865d3e279c4e8eaa1af87b
Izet Delibasic <izetdelibasic55@gmail.com>
2024-06-02 19:17:58 +0200

    Resolving link problems

--- src/components/LandingAction/LandingAction.jsx
+++ src/components/LandingAction/LandingAction.jsx
@@ -2,31 +2,30 @@
 import { Link } from "react-router-dom";
 // - Components -
 import { CustomButton } from "..";
-// -Icons-
+// - Icons -
 import { IoArrowForwardSharp } from "react-icons/io5";
 
 const LandingAction = () => {
   return (
     <div className="flex flex-col items-center justify-center sm:flex-row md:justify-center md:w-[30rem] lg:w-full w-[90%] mx-auto md:mx-0">
-      <Link href="/register">
+      <Link to="/register">
         <CustomButton
           className="relative bg-blue-500 text-white font-medium py-[1rem] lg:px-[2.5rem] px-[2.5rem] md:px-[1.5rem] mr-0 mb-[20px] sm:mb-0 rounded-[3rem] group overflow-hidden z-[1] text-nowrap"
           iconClassName="group-hover:text-white ml-[10px]"
           title="Start Journey"
           titleClassName="group-hover:text-white font-subtitle"
-          href="/register"
           Icon={IoArrowForwardSharp}
         >
           <div className="absolute inset-0 bg-black w-full transform origin-right transition-transform duration-300 group-hover:scale-x-0 z-[-1]"></div>
         </CustomButton>
       </Link>
-      <Link href="/register">
+      <Link to="/login">
         <CustomButton
           className="relative bg-blue-500 text-white font-medium py-[1rem] lg:px-[2.5rem] px-[2.5rem] md:px-[1.5rem] rounded-[3rem] group overflow-hidden z-[1] sm:ml-[20px] hover:border-opacity-0 hover:border-transparent text-nowrap"
           iconClassName=""
           title="Search Your Way"
           titleClassName="group-hover:text-white font-subtitle"
-          href="/login"
+          Icon={IoArrowForwardSharp}
         >
           <div className="absolute inset-0 bg-black w-full transform origin-right transition-transform duration-300 group-hover:scale-x-0 z-[-1]"></div>
         </CustomButton>
