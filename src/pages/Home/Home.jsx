e21f3df2306aafb7a4764c87b32b83c2a6148678
Izet Delibasic <izetdelibasic55@gmail.com>
2024-06-02 19:11:31 +0200

    Fixing deploying issue

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 // -React-
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Landing from "../Landing/Landing";
 
 const Home = () => {
   return (
     <div>
-      <p>Home</p>
-      <Outlet />
+      <Landing />
     </div>
   );
 };
