ffa24bf7cf6a0daacd2cbfd273da3d0d6a9c9a40
Izet Delibasic <izetdelibasic55@gmail.com>
2024-06-05 20:46:40 +0200

    Adding style for premium routes

--- src/pages/AllRoutes/AllRoutes.jsx
+++ src/pages/AllRoutes/AllRoutes.jsx
@@ -114,81 +114,80 @@ const AllRoutes = () => {
       </h1>
       <div className="grid grid-cols-1 font-cabin mx-auto p-4">
         {data.length > 0 ? (
-          data.map(
-            (item, index) =>
-              item.route.seatsNumber !== 0 && (
-                <div
-                  className="bg-white rounded-lg shadow-md p-6 mb-4 lg:mr-4 text-center lg:w-[95%] w-[90%] mx-auto border-[1px] border-opacity-25 border-black hover:border-blueColor ease-in-out duration-300"
-                  key={index}
-                >
-                  <div className="mb-4">
-                    <div className="flex sm:flex-row flex-col justify-between border-b-2">
-                      <h2 className="text-xl font-bold">
-                        {item.user.firstName} {item.user.lastName} -{" "}
-                        {item.route.type}
-                      </h2>
-                      <p className="text-gray-600">
-                        Kontakt: {item.user.phoneNumber}
-                      </p>
-                    </div>
-                  </div>
-                  <div className="border-b-2">
-                    <h3 className="text-lg font-medium mb-2 md:text-[2.5rem] text-[1.8rem]">
-                      {item.route.name}
-                    </h3>
-                    <div className="flex sm:flex-row justify-between items-center mb-2">
-                      <div className="flex flex-col">
-                        {item.route.type === "Package" ? (
-                          <div className="flex">
-                            <p className="font-semibold mr-1">Packages:</p>
-                            <p className="text-greenColor">
-                              {item.route.seatsNumber}
-                            </p>
-                          </div>
-                        ) : (
-                          <div className="flex">
-                            <p className="font-semibold mr-1">Seats:</p>
-                            <p className="text-greenColor">
-                              {item.route.seatsNumber}
-                            </p>
-                          </div>
-                        )}
-                        <p className="sm:text-[2rem] text-greenColor">
-                          {item.route.price}€
-                        </p>
-                      </div>
-                      <div className="flex flex-col text-greenColor sm:text-[1.5rem]">
-                        <p>
-                          {new Date(item.route.dateAndTime).toLocaleDateString(
-                            "en-GB",
-                            {
-                              day: "numeric",
-                              month: "long",
-                            }
-                          )}
-                        </p>
-                        <p>
-                          {new Date(item.route.dateAndTime).toLocaleTimeString(
-                            "en-GB",
-                            {
-                              hour: "numeric",
-                              minute: "numeric",
-                            }
-                          )}
-                        </p>
-                      </div>
-                    </div>
-                    <p className="sm:text-[1.2rem]">{item.route.description}</p>
-                  </div>
-                  <button
-                    onClick={() => handleButtonClick(item.route, item.user)}
-                    className="bg-blueColor text-white font-medium py-2 px-10 mt-4 rounded-lg hover:bg-lightBlue transition duration-300 ease-in-out"
-                  >
-                    Send Request
-                  </button>
+          data.map((item, index) => (
+            <div
+              className={`bg-white rounded-lg shadow-md p-6 mb-4 lg:mr-4 text-center lg:w-[95%] w-[90%] mx-auto border-[1px] border-opacity-25 border-black hover:border-blueColor ease-in-out duration-300 ${
+                index < 2 ? "border-yellow-600 border-2" : ""
+              }`}
+              key={index}
+            >
+              <div className="mb-4">
+                <div className="flex sm:flex-row flex-col justify-between border-b-2">
+                  <h2 className="text-xl font-bold">
+                    {item.user.firstName} {item.user.lastName} -{" "}
+                    {item.route.type}
+                  </h2>
+                  <p className="text-gray-600">
+                    Kontakt: {item.user.phoneNumber}
+                  </p>
                 </div>
-              )
-          )
+              </div>
+              <div className="border-b-2">
+                <h3 className="text-lg font-medium mb-2 md:text-[2.5rem] text-[1.8rem]">
+                  {item.route.name}
+                </h3>
+                <div className="flex sm:flex-row justify-between items-center mb-2">
+                  <div className="flex flex-col">
+                    {item.route.type === "Package" ? (
+                      <div className="flex">
+                        <p className="font-semibold mr-1">Packages:</p>
+                        <p className="text-greenColor">
+                          {item.route.seatsNumber}
+                        </p>
+                      </div>
+                    ) : (
+                      <div className="flex">
+                        <p className="font-semibold mr-1">Seats:</p>
+                        <p className="text-greenColor">
+                          {item.route.seatsNumber}
+                        </p>
+                      </div>
+                    )}
+                    <p className="sm:text-[2rem] text-greenColor">
+                      {item.route.price}€
+                    </p>
+                  </div>
+                  <div className="flex flex-col text-greenColor sm:text-[1.5rem]">
+                    <p>
+                      {new Date(item.route.dateAndTime).toLocaleDateString(
+                        "en-GB",
+                        {
+                          day: "numeric",
+                          month: "long",
+                        }
+                      )}
+                    </p>
+                    <p>
+                      {new Date(item.route.dateAndTime).toLocaleTimeString(
+                        "en-GB",
+                        {
+                          hour: "numeric",
+                          minute: "numeric",
+                        }
+                      )}
+                    </p>
+                  </div>
+                </div>
+                <p className="sm:text-[1.2rem]">{item.route.description}</p>
+              </div>
+              <button
+                onClick={() => handleButtonClick(item.route, item.user)}
+                className="bg-blueColor text-white font-medium py-2 px-10 mt-4 rounded-lg hover:bg-lightBlue transition duration-300 ease-in-out"
+              >
+                Send Request
+              </button>
+            </div>
+          ))
         ) : (
           <p className="text-center">Empty array.</p>
         )}
